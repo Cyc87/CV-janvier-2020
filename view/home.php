8331abb8e6cadc59d7cef14197aacdb7e25cbb3b
Cyc87 <chenalcyril87@gmail.com>
2020-01-20 18:58:01 +0100

    set up controler, index, public, view

---
+++ view/home.php
@@ -0,0 +1,13 @@
+<?php $title = 'CV Cyril Chenal'; ?>
+
+<?php ob_start(); ?>
+
+<?php require("view/menu.php"); ?>
+
+
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/template.php') ?>
+<?php require('view/footer.php') ?>
---
+++ view/menu.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="public/logo/logo cyc.png" alt="logo cv cyril chenal developpeur web" style="width:8%">
+        <a class="navbar-brand">Cyril Chenal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Hello <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Compétences</a>
+                <a class="nav-item nav-link" href="#">Dîplomes</a>
+                <a class="nav-item nav-link" href="#">Portfolio</a>
+                <a class="nav-item nav-link" href="#">Expérience</a>
+                <a class="nav-item nav-link" href="#">loisirs</a>
+                <a class="nav-item nav-link" href="#">Contact</a>
+            </div>
+        </div>
+    </nav>
+</header>
